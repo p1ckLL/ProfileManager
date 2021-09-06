b361714bff9a084bda91b4e1d7288efdd6029747
p1ckLL <86033747+p1ckLL@users.noreply.github.com>
2021-09-06 11:36:56 -0700

    changed PickleStore references to ProfileManager
    oops

--- ProfileManager.lua
+++ ProfileManager.lua
@@ -1,99 +1,39 @@
-local ProfileService = require(script.ProfileService)
+-- NOTE: The way I use these functions is not necessarily ideal, 
+-- they are just examples of how you could use them
+local ProfileManager = require(game.ServerScriptService.ProfileManager)
 
-local ProfileStores = {
-	Player = ProfileService.GetProfileStore(
-		"Player",
-		{
-			money = 0;
-		}
-	),
-	Custom = ProfileService.GetProfileStore(
-		"Custom",
-		{
-			bans = {}
-		}
-	)
-}
-
-local cachedProfiles = {}
-
-game.Players.PlayerRemoving:Connect(function(plr)
-	local key = "Player_"..plr.UserId
-	local profile = cachedProfiles[key]
+game.Players.PlayerAdded:Connect(function(plr)
+	local plrProfile = ProfileManager:LoadPlayerProfile(plr)
+	local plrData = ProfileManager:GetDataFromProfile(plrProfile)
 	
-	if profile then
-		profile:Release()
-	end
+	print("This should print a table with atleast the default player values:")
+	print(plrData)
+	
+	local bansProfile = ProfileManager:LoadCustomProfile("Custom", "Custom_"..game.PlaceId)
+	local bansData = ProfileManager:GetDataFromProfile(bansProfile)
+	
+	print("This should print a table with the banned people:")
+	print(bansData)
+	
+	bansData.bans[plr.Name] = true
+	ProfileManager:ReleaseProfile(bansProfile)
+	
+	plr.Chatted:Connect(function(msg)
+		local requestedPlrId = game.Players:GetUserIdFromNameAsync(msg)
+		local requestedPlr = game.Players:GetPlayerByUserId(requestedPlrId)
+		
+		if requestedPlr and requestedPlr:IsDescendantOf(game.Players) then
+			local key = "Player_"..requestedPlr.UserId
+			local requestedPlrProfile = ProfileManager:GetProfile(key)
+			local requestedPlrData = ProfileManager:GetDataFromProfile(requestedPlrProfile)
+			
+			print(requestedPlrData)
+			
+			ProfileManager:ReleaseProfile(requestedPlrProfile)
+		end
+	end)
 end)
 
-local PickleStore = {}
-
-function PickleStore:ReleaseProfile(profile)
-	profile:Release()
-end
-
-function PickleStore:ReleaseAllProfiles(profile)
-	for _, profile in pairs(cachedProfiles) do
-		profile:Release()
-	end
-end
-
-function PickleStore:GetProfile(key)
-	local profile = cachedProfiles[key]
-	
-	if profile then
-		return profile
-	end
-end
-
-function PickleStore:GetDataFromProfile(profile)
-	if profile then
-		return profile.Data
-	end
-end
-
-function PickleStore:SetupProfile(profile, key)
-	if profile then
-		profile:Reconcile()
-		profile:ListenToRelease(function()
-			cachedProfiles[key] = nil
-		end)
-		
-		cachedProfiles[key] = profile
-		return true
-	end
-end
-
-function PickleStore:LoadPlayerProfile(plr)
-	local key = "Player_"..plr.UserId
-	
-	local profile = ProfileStores["Player"]:LoadProfileAsync(
-		key,
-		"ForceLoad"
-	)
-	
-	local setupSuccess = PickleStore:SetupProfile(profile, key)
-
-	if setupSuccess then
-		return profile
-	else
-		warn("Error finding/setting up profile.")
-	end
-end
-
-function PickleStore:LoadCustomProfile(profileName, key)
-	local profile = ProfileStores["Custom"]:LoadProfileAsync(
-		key,
-		"ForceLoad"
-	)
-	
-	local setupSuccess = PickleStore:SetupProfile(profile, key)
-	
-	if setupSuccess then
-		return profile
-	else
-		warn("Error finding/setting up profile.")
-	end
-end
-
-return PickleStore
+game:BindToClose(function()
+	ProfileManager:ReleaseAllProfiles()
+end)
